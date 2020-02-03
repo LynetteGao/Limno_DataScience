3ad181a19301ff9965e0d90cf85b0ab14c9fe6ff
hydrobert <ladwigjena@gmail.com>
2020-02-03 15:02:11 -0600

    sovled time issue, results are now in a plausible range (0-15 mg/l), still problems with 1980 and 1989 as there is no thermocline during these years

--- oxyModel.R
+++ oxyModel.R
@@ -35,35 +35,36 @@ for (ii in lks[1]){
   # temperature conversion there
   input.values <- input(wtemp = data, H = H, A = A)
   
-  fsed_stratifed = 0.01 *100
+  fsed_stratified = 0.01 *100
   fsed_not_stratified  =  0.0002
-  nep_stratified = max(A)*2
+  nep_stratified = 1#max(A)*2
   nep_not_stratified = 0
   
-  o2<- calc_do(input.values = input.values,fsed_stratifed,fsed_not_stratified,nep_stratified,nep_not_stratified)
+  o2<- calc_do(input.values = input.values,fsed_stratified,fsed_not_stratified,nep_stratified,nep_not_stratified)
   input.values$o2_epil <- o2[,"o2_epil"]
   input.values$o2_hypo <- o2[,"o2_hypo"]
   input.values$o2_total <- o2[,"o2_total"]
 }
 
-input.values$year <- year(space_time$datetime)
-input.values$doy <- yday(space_time$datetime)
+input.values$year <- year(input.values$datetime)
+input.values$doy <- yday(input.values$datetime)
 
 ggplot(subset(input.values, year == '1979')) +
   geom_line(aes(doy, td.depth, col = 'Total')) 
 
-ggplot(subset(input.values, year == '1979')) +
-  geom_line(aes(doy, log10(o2_total), col = 'Total')) +
-  geom_line(aes(doy, log10(o2_epil), col = 'Epi')) +
-  geom_line(aes(doy, log10(o2_hypo), col = 'Hypo')) +
+ggplot(subset(input.values, year == '1995')) +
+ # geom_line(aes(doy, (o2_total/total_vol/1000), col = 'Total')) +
+  geom_line(aes(doy, (o2_epil/vol_epil/1000), col = 'Epi')) +
+  geom_line(aes(doy, (o2_hypo/vol_hypo/1000), col = 'Hypo')) +
   # ylim(-10, 1e30)+
   facet_wrap(~year) +
   theme_bw()
 
 ggplot(input.values) +
-  geom_point(aes(doy, (o2_total), col = 'Total')) +
-  geom_point(aes(doy, (o2_epil), col = 'Epi')) +
-  geom_point(aes(doy, (o2_hypo), col = 'Hypo')) +
+  geom_point(aes(doy, (o2_total/total_vol/1000), col = 'Total')) +
+  geom_point(aes(doy, (o2_epil/vol_epil/1000), col = 'Epi')) +
+  geom_point(aes(doy, (o2_hypo/vol_hypo/1000), col = 'Hypo')) +
+  ylim(0,20)+
   facet_wrap(~year) +
   theme_bw()
 
